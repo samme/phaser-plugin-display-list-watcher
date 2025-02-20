3401f8cdec2201ac1d59673ee144678f71a0251f
samme <samme@users.noreply.github.com>
2025-02-20 11:51:19 -0800

    test multiple cameras etc.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import Phaser from 'phaser'
 import DisplayListWatcher from './src/main'
 
+function stringToColor(str) {
+  let hash = 5381
+
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash * 33) ^ str.charCodeAt(i)
+  }
+
+  return hash >>> (0 % 0xffffff)
+}
+
 class Example extends Phaser.Scene {
+  init() {
+    this.cameras
+      .add(512, 384, 512, 384)
+      .setAngle(45)
+      .setAlpha(0.5)
+      .setBackgroundColor(stringToColor(this.sys.settings.key))
+      .centerOn(0, 0)
+  }
+
   preload() {}
 
   create() {
@@ -10,10 +29,12 @@ class Example extends Phaser.Scene {
     this.add.layer([this.add.layer(this.add.layer())])
     this.add.particles(0, 0, '__DEFAULT')
     this.add.zone(0, 0, 1, 1)
+    this.add.text(0, 0, '👀', { font: '72px sans-serif' }).setName('eyes')
   }
 }
 
 new Phaser.Game({
+  // type: Phaser.CANVAS,
   plugins: {
     scene: [
       {
@@ -32,6 +53,7 @@ new Phaser.Game({
       key: '5 no other plugins',
       active: true,
       plugins: ['DisplayListWatcher']
-    })
+    }),
+    new Example({ key: '6 active', active: true })
   ]
 })
